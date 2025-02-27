acf6857163ca23096caaea922a3a0fbc4e532544
Caio Alves <j4c.2@hotmail.com>
2025-02-27 02:41:49 -0300

    :sparkles: feat: Tela de registro criada

--- screens/auth/register/index.tsx
+++ screens/auth/register/index.tsx
@@ -1,76 +1,56 @@
-import { useState } from "react";
 import { router } from "expo-router";
+import { SafeAreaView } from "react-native";
 
 import { HStack } from "@/components/ui/hstack";
 import { VStack } from "@/components/ui/vstack";
 import { Text } from "@/components/ui/text";
-
-import { ArrowLeft } from 'phosphor-react-native'
-import { TouchableOpacity } from "react-native";
+import { Avatar, AvatarBadge, AvatarImage } from "@/components/ui/avatar";
 
 import { CustomInput } from '@/components/custom/input'
 import { CustomButton } from '@/components/custom/button'
-import { Selection } from '@/components/custom/selection'
-import { TagFilter } from '@/components/custom/tagFilter'
-import Toggle from '@/components/custom/toggle'
-import ProductCard from "@/components/custom/card";
+
+import { MarketspaceRoundLogo } from "@/assets/svg/marketspaceRoundLogo";
+
+import { PencilSimpleLine } from 'phosphor-react-native'
 
 
 export default function Register() {
-    const [checkSelected, setCheckSelected] = useState(false)
-    const [radioSelected, setRadioSelected] = useState(false)
-
     return (
-        <VStack className="flex-1 justify-center items-center bg-color-gray-3 gap-4 bg-gray-6 px-12">
-            <TouchableOpacity 
-            className="justify-center bg-gray-5 p-2 rounded-md"
-            onPress={() => router.back()}
-            >
-                <HStack className="gap-2 p-1 items-center">
-                    <ArrowLeft size={16}/>
-                    <Text className="text-base text-black">
-                        Voltar
-                    </Text>
-                </HStack>
-            </TouchableOpacity>
-            <Text className="text-xl font-bold text-black">TELA DE REGISTRO</Text>
-            <Text className="text-lg font-bold text-black">Testando componentes</Text>
-            <CustomInput/>
-            <CustomButton
-            type="blue"
-            iconName="Plus"
-            fullWidth
-            onPress={() => console.log('passou aqui')}
-            > 
-            Button
-            </CustomButton>
-            <Selection 
-            label="Teste" 
-            selected={checkSelected} 
-            onPress={() => setCheckSelected(!checkSelected)} 
-            type="checkbox"
-            />
-            <Selection 
-            label="Teste" 
-            selected={radioSelected} 
-            onPress={() => setRadioSelected(!radioSelected)} 
-            type="radio"
-            />
-            <HStack className="gap-2">
-            <TagFilter label="NOVO"/>
-            <TagFilter label="USADO"/>
-            </HStack>
-            <Toggle/>
-            <VStack>
-            <HStack className="gap-5">
-            <ProductCard/>
-            <ProductCard isUsed/>
-            </HStack>
-            <HStack className="gap-5">
-            <ProductCard isDeactivated/>
-            <ProductCard isDeactivated isUsed/>
-            </HStack>
-            
+        <VStack className="flex-1 bg-gray-6 gap-8 justify-center px-10 pt-6">
+            <VStack className="items-center gap-3">
+                <MarketspaceRoundLogo />
+                <VStack className="items-center gap-2">
+                    <Text className="font-bold text-gray-1 text-xl">Boas vindas!</Text>
+                    <Text className="font-karla text-gray-2 text-md text-center">Crie sua conta e use o espaço para comprar itens variados e vender seus produtos</Text>
+                </VStack>
+            </VStack>
+            <VStack className="w-full bg-color-gray-3 items-center justify-center">
+                <VStack className="w-full items-center gap-6">
+                    <VStack className="w-full items-center gap-4">
+                        <Avatar size="xl">
+                            <AvatarImage
+                                source={{ uri: 'https://avatars.githubusercontent.com/u/47273082?v=4' }}
+                                alt="avatar"
+                                className="border-[3px] border-blue-light"
+                            />
+                            <AvatarBadge className="absolute bottom-0 right-0 bg-blue-light border-blue-light items-center justify-center p-4">
+                                <PencilSimpleLine size={16} color="#F7F7FB" />
+                            </AvatarBadge>
+                        </Avatar>
+                        <CustomInput placeholder="Nome" />
+                        <CustomInput placeholder="E-mail" />
+                        <CustomInput placeholder="Telefone" />
+                        <CustomInput placeholder="Senha" type="password" />
+                        <CustomInput placeholder="Confirmar senha" type="password" />
+                    </VStack>
+                    <CustomButton type="black" fullWidth onPress={() => router.replace('/')}>Criar</CustomButton>
+                </VStack>
+            </VStack>
+            <VStack className="w-full items-center gap-4 pt-10">
+                <Text className="text-gray-1 text-sm">
+                    Já possui uma conta?
+                </Text>
+                <CustomButton type="gray" fullWidth onPress={() => router.replace('/')}>Ir para o login</CustomButton>
             </VStack>
         </VStack>
     )
