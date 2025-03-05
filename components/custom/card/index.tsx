2ca68e0f6ad9cca6467177a91aa5d4c9781c1790
Caio Alves <j4c.2@hotmail.com>
2025-03-05 20:16:34 -0300

    :bug: fix: Componentes agora são dinamicos

--- components/custom/card/index.tsx
+++ components/custom/card/index.tsx
@@ -6,12 +6,17 @@ import { HStack } from "@/components/ui/hstack";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 interface ProductCardProps {
+  avatar: string;
+  title: string;
+  price: number;
+  prodImage?: string;
   className?: string;
   isUsed?: boolean;
   isDeactivated?: boolean;
 }
 
-export default function ProductCard({ isDeactivated, className, isUsed = false }: ProductCardProps) {
+export function ProductCard({ avatar, title, price, prodImage, isDeactivated, className, isUsed = false }: ProductCardProps) {
+  const imageAvatar = require('../../../assets/images/user1.jpg');
   return (
     <TouchableOpacity className={`w-[150px] ${className}`}>
       <View className="relative">
@@ -21,24 +26,26 @@ export default function ProductCard({ isDeactivated, className, isUsed = false }
         </View>
       )}
         <HStack className="absolute top-0 left-0 right-0 p-2 z-10 flex-row justify-between">
-          <Avatar size="sm" >
-            <AvatarImage source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}/>
-          </Avatar>
+          <Image
+          source={avatar}
+          alt="Imagem do usuário"
+          className="w-8 h-8 rounded-full border-[1px] border-gray-7"
+          />
           <Badge className={`absolute top-2 right-2 px-2 py-1 rounded-full ${isUsed ? 'bg-gray-2' : 'bg-blue'}`}>
             <Text className="font-bold text-[10px]">{isUsed ? "USADO" : "NOVO"}</Text>
           </Badge>
         </HStack>
         <Image
-          source={require('../../../assets/images/tenis.png')}
-          alt="Tênis vermelho"
+          source={prodImage}
+          alt="Imagem do produto"
           size="none"
           className="aspect-[153/100] w-full rounded-lg"
         />
       </View>
       <View className="p-2">
-        <Text className={`text-sm font-karla ${isDeactivated ? 'text-gray-4' : 'text-gray-2'}`}>Tênis vermelho</Text>
+        <Text className={`text-sm font-karla ${isDeactivated ? 'text-gray-4' : 'text-gray-2'}`}>{title}</Text>
         <HStack className="items-center gap-1">
-          <Text className={`text-base ${isDeactivated ? 'font-karl text-gray-4' : 'font-bold text-gray-1'}`}>R$ 59,90</Text>
+          <Text className={`text-base ${isDeactivated ? 'font-karl text-gray-4' : 'font-bold text-gray-1'}`}>R$ {price.toFixed(2)}</Text>
         </HStack>
       </View>
     </TouchableOpacity>
--- components/custom/input/index.tsx
+++ components/custom/input/index.tsx
@@ -1,31 +1,48 @@
 import { HStack } from '@/components/ui/hstack'
-import { Eye } from 'phosphor-react-native';
+import { Divider } from '@/components/ui/divider'
+
+import { Eye, MagnifyingGlass, Sliders } from 'phosphor-react-native';
 import { useState } from 'react';
-import { TextInput, TouchableOpacity }  from 'react-native'
+import { TextInput, TouchableOpacity, View } from 'react-native'
 
 interface CustomInputProps {
     placeholder?: string;
-    type?: 'text' | 'password';
-  }
+    type?: 'text' | 'password' | 'search';
+}
 
 export function CustomInput({ placeholder, type }: CustomInputProps) {
     const [isFocused, setIsFocused] = useState(false);
     const [showPassword, setShowPassword] = useState(type === 'password');
-    return(
-        <HStack className='w-full'> 
-        <TextInput className={`w-full bg-gray-7 rounded-md px-4 py-3 placeholder:font-karla placeholder:text-gray-4 placeholder:text-base ${isFocused ? 'border-[1px] border-gray-3' : ''}`}
-        placeholder={placeholder ? placeholder : 'Digite algo'} 
-        secureTextEntry={!showPassword} 
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
-        >
-            
-        </TextInput>
-        { type === 'password' &&
-            <TouchableOpacity className="absolute right-4 top-3" onPress={() => setShowPassword(!showPassword)}>
-            <Eye size={24} color="#9CA3AF" />
-            </TouchableOpacity>
-        }
+
+    return (
+        <HStack className="w-full bg-gray-7 rounded-md px-4 py-3 items-center border-gray-3"
+            style={{ borderWidth: isFocused ? 1 : 0 }}>
+
+            <TextInput
+                className="flex-1 placeholder:font-karla placeholder:text-gray-4 placeholder:text-base"
+                placeholder={placeholder ?? 'Digite algo'}
+                secureTextEntry={type === 'password' && !showPassword}
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setIsFocused(false)}
+            />
+
+            {type === 'password' && (
+                <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                    <Eye size={24} color="#9CA3AF" />
+                </TouchableOpacity>
+            )}
+
+            {type === 'search' && (
+                <HStack className="ml-2 items-center min-h-[24px]">
+                    <TouchableOpacity onPress={() => { }}>
+                        <MagnifyingGlass size={24} color="#9CA3AF" />
+                    </TouchableOpacity>
+                    <Divider orientation="vertical" className="mx-2 bg-gray-4 w-[1px] h-5" />
+                    <TouchableOpacity onPress={() => { }}>
+                        <Sliders size={24} color="#9CA3AF" />
+                    </TouchableOpacity>
+                </HStack>
+            )}
         </HStack>
-    )
+    );
 }
