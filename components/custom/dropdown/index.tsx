7753c2bad003b61e5307d1358a2c3de12ed32921
Caio Alves <j4c.2@hotmail.com>
2025-03-08 17:48:41 -0300

    :sparkles: feat: Componente dropdown criado

---
+++ components/custom/dropdown/index.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { Text, TouchableOpacity, FlatList } from 'react-native';
+
+import { VStack } from '@/components/ui/vstack';
+import { HStack } from '@/components/ui/hstack';
+
+import { CaretDown } from 'phosphor-react-native'
+
+interface DropdownProps {
+    options: string[];
+    selected: string | null;
+    onSelect: (option: string) => void;
+}
+
+export function Dropdown({ options, selected, onSelect }: DropdownProps) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleSelect = (option: string) => {
+        onSelect(option);
+        setIsOpen(false);
+    };
+
+    return (
+        <VStack className="relative flex-row">
+            <TouchableOpacity
+                onPress={() => setIsOpen(!isOpen)}
+                className="bg-gray-7 px-3 py-2 rounded-md border border-gray-5">
+                <HStack className='gap-2'>
+                    <Text className="text-gray-1 font-karl text-md">{selected}</Text>
+                    <CaretDown size={16} color="black" />
+                </HStack>
+            </TouchableOpacity>
+
+            {isOpen && (
+                <VStack className="absolute mt-2 bg-white rounded-md w-full">
+                    <FlatList
+                        data={options}
+                        renderItem={({ item }) => (
+                            <TouchableOpacity
+                                onPress={() => handleSelect(item)}
+                                className="p-3">
+                                <Text className="text-gray-700">{item}</Text>
+                            </TouchableOpacity>
+                        )}
+                        keyExtractor={(item) => item}
+                    />
+                </VStack>
+            )}
+        </VStack>
+    );
+};
--- screens/main/myAds/index.tsx
+++ screens/main/myAds/index.tsx
@@ -1,17 +1,24 @@
 import { useEffect, useState } from "react";
-import { 
-  KeyboardAvoidingView, 
-  Platform, 
+import {
+  KeyboardAvoidingView,
+  Platform,
   Keyboard,
-  TouchableWithoutFeedback, 
-  ScrollView
+  TouchableWithoutFeedback,
+  FlatList,
+  TouchableOpacity
 } from "react-native";
 
 import { Text } from "@/components/ui/text";
 import { VStack } from "@/components/ui/vstack";
+import { HStack } from "@/components/ui/hstack";
 
-export default function MyAds() {;
+import { CaretDown, Plus } from "phosphor-react-native";
+import { Dropdown } from "@/components/custom/dropdown";
+
+export default function MyAds() {
   const [keyboardVisible, setKeyboardVisible] = useState(false);
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+  const options = ['Todos', 'Ativos', 'Inativos'];
 
   useEffect(() => {
     const showSubscription = Keyboard.addListener("keyboardDidShow", () => setKeyboardVisible(true));
@@ -24,21 +31,40 @@ export default function MyAds() {;
   }, []);
 
   return (
-    <KeyboardAvoidingView 
-      className="flex-1" 
+    <KeyboardAvoidingView
+      className="flex-1 w-full bg-gray-6 px-12 py-16 justify-start"
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0}
     >
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <ScrollView 
-          className="flex-1"
-          contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }}
+        <FlatList
+          data={[{}]} // FlatList espera um array, então passamos um item vazio só para renderizar o conteúdo
+          keyExtractor={() => "content"}
+          contentContainerStyle={{ flexGrow: 1 }}
           keyboardShouldPersistTaps="handled"
-        >
-            <VStack className="flex-1 items-center justify-center bg-gray-5 w-full">
-            <Text className="text-xl font-bold text-gray-1">TESTE 2</Text>
+          showsVerticalScrollIndicator={false}
+          renderItem={() => (
+            <VStack className="items-start justify-start gap-8">
+              {/* Header */}
+              <HStack className="flex-row items-center w-full justify-center">
+                <Text className="font-bold text-gray-1 text-lg">Meus anúncios</Text>
+                <TouchableOpacity className="absolute right-0">
+                  <Plus size={24} color="#3E3A40" />
+                </TouchableOpacity>
+              </HStack>
+
+              {/* Header Frame 2 */}
+              <HStack className="flex-row items-center w-full justify-between">
+                <Text className="text-gray-2 text-md">9 anúncios</Text>
+                <Dropdown 
+                  options={options} 
+                  selected={selectedOption} 
+                  onSelect={setSelectedOption} 
+                />
+              </HStack>
             </VStack>
-        </ScrollView>
+          )}
+        />
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
   );
